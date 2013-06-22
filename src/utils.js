1fd0440e1a83e6012e814b6a511155d9dc380a72
Derek Chiang <derekchiang93@gmail.com>
2013-06-22 13:55:29 -0700

    Add utils functions

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+exports.hash = require('./hash').hash
+
+function generateID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+exports.generateID = generateID
